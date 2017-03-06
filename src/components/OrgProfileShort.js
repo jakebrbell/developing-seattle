9ec37adc1bae2c4d877be85d7e4c00ab336a7cfa
Jake Bell <jakebrbell@gmail.com>
2017-03-05 19:11:09 -0800

    Close #11 Create OrgProfileShort component

---
+++ src/components/OrgProfileShort.js
@@ -0,0 +1,61 @@
+import { Button, Col, Image, Label, Row } from 'react-bootstrap';
+import React from 'react';
+
+const OrgProfileShort = ({ org }) => {
+  const styles = {
+    label: {
+      backgroundColor: '#8ED4CC',
+      boxShadow: '3px 3px 7px -1px rgba(0,0,0,0.52)'
+    },
+    labelWrapper: {
+      display: 'inline',
+      marginRight: '10px'
+    },
+    link: {
+      color: 'black'
+    },
+    logo: {
+      display: 'block',
+      height: '200px',
+      margin: '20px auto',
+      background: '#fff'
+    },
+    row: {
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+      borderRadius: '10px',
+      marginBottom: '30px',
+      padding: '30px'
+    }
+  };
+
+  return (
+    <Row style={styles.row}>
+      <Col xs={12}>
+        <a style={styles.link}>
+          <Col xs={12} sm={3} style={{ padding: 0 }}>
+            <Image
+              circle
+              src={`assets/images/${org.logoUrl}`}
+              style={styles.logo}
+            />
+            <Button block bsStyle="primary">View Full Profile</Button>
+          </Col>
+          <Col xs={12} sm={9} style={{ paddingLeft: 50 }}>
+            <h2>{org.name}</h2>
+            <p>{org.address}</p>
+            <div style={{ marginBottom: 15 }}>
+              {org.causes.map((cause, index) => {
+                return <h3 key={index} style={styles.labelWrapper}>
+                  <Label style={styles.label}>{cause}</Label>
+                </h3>;
+              })}
+            </div>
+            <p>{org.desc}</p>
+          </Col>
+        </a>
+      </Col>
+    </Row>
+  );
+}
+
+export default OrgProfileShort;
