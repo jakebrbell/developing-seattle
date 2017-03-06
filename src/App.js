a550d3873e9686095fa1f9e619c30efc1a304532
Jake Bell <jakebrbell@gmail.com>
2017-03-05 17:43:25 -0800

    Add HomeContainer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import MainNav from './components/MainNav';
+import HomeContainer from './components/HomeContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <MainNav />
+        <main style={{ minHeight: 'calc(100vh - 170px)' }}>
+          <HomeContainer />
+        </main>
       </div>
     );
   }
